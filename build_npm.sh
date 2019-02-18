bd47ba8576de7cf55cefc7339f48a8a6b97ce930
eodell <eocom@kyrillia.com>
2019-02-18 00:46:41 -0600

    npm install works on both Windows/*nix

--- build_npm.sh
+++ build_npm.sh
@@ -3,6 +3,7 @@
 # This script builds a gzipped tarball suitable for use by npm as a local package.
 # It is assumed that ../cephalopov holds the contents for the package.
 
+rm -f cephalopov.tgz
 cd ..
 tar -czvf cpov_examples/cephalopov.tgz --exclude=.* cephalopov
 cd cpov_examples
---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+
+# This script deletes all of the generated files in preparation for distribution.
+
+rm -f cephalopov.tgz
